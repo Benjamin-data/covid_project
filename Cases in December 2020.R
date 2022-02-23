990d9e1b1b96526698a854f23d00f58f6253671a
baindoux <99143825+baindoux@users.noreply.github.com>
2022-02-23 14:13:43 -0500

    Create Cases in December 2020.R

---
+++ Cases in December 2020.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+################################################################################
+
+# "Confirmed positive cases of COVID-19 in Ontario"
+
+#> covid_19 is a dataset that compiles daily snapshots of publicly reported data
+#> on 2019 Novel Coronavirus (COVID-19) testing in Ontario.
+
+#> Source: 
+#> https://open.canada.ca/data/en/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27
+
+################################################################################
+
+# Number of confirmed positive cases of COVID_19 reported on December, 2020 (first try with a small scale)
+
+# Using subset to filter the data only corresponding to the month of December 2020 
+(covid_19_december_2020<- subset(covid_19, Case_Reported_Date> "2020-12-01" 
+                                 & Case_Reported_Date < "2020-12-31"))
+
+# Create a bar for December, 2020
+ggplot(data = covid_19_december_2020) + 
+  geom_bar(
+    mapping = aes(x = Case_Reported_Date, fill=Age_Group) #Representation of the genders with fill
+  ) +
+  labs(
+    x = 'Date',
+    y = 'Confirmed positive cases',
+    fill = 'Age group',
+    title = "Confirmed positive cases (COVID-19) reported in Ontario in December 2020",
+    caption = "Source: https://open.canada.ca/data/en/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27"
+    )
